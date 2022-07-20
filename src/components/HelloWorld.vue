3814002020d39cdd3da34f04d81c0f813ddeb4f7
jakubserwin <kuba.serwin99@gmail.com>
2022-07-20 13:40:18 +0200

    theme, aliases and eslint config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { useDark, useToggle } from '@vueuse/core'
 
-defineProps<{ msg: string }>()
-
-const test = () => true
-
-let b = 2
-
-b = 'test'
-
-const count = ref(0)
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1>isDark : {{ isDark }}</h1>
+  <button @click="toggleDark()">
+    toggle theme
+  </button>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
