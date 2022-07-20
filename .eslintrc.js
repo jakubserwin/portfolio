1f9b2870bc0e6183a9c84a652d8c754e7a79f7d8
jakub.serwin <j.serwin@digitalcolliers.com>
2022-07-20 11:42:14 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+}
