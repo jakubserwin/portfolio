dd782ccaa2689fbbb331b646ee20d71909e3bd2d
jakubserwin <kuba.serwin99@gmail.com>
2022-07-26 19:49:32 +0200

    move animations to separate file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { onBeforeMount } from "vue";
 import ThemeSwitch from "@/components/UI/ThemeSwitch/ThemeSwitch.vue";
+
+onBeforeMount(() => {
+  if (!window.sessionStorage.getItem('animated')) {
+    const body: HTMLBodyElement | null = document.querySelector('body')
+    if(body)
+      body.classList.add('animate');
+    window.sessionStorage.setItem('animated', 'active');
+  }
+})
 </script>
 
 <template>
